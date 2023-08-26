d016c51765a69a0cd7e99cf0e2f88182e4bb1180
Mikhail Melikhov <melihov@me.com>
2023-08-26 14:17:39 +0300

    Create 189.RotateArray.swift

---
+++ 189.RotateArray.swift
@@ -0,0 +1,16 @@
+/*
+
+Given an integer array 'nums', rotate the array to the right by 'k'
+steps, where 'k' is non-negative.
+
+*/
+
+class Solution {
+	func rotate(_ nums: inout [Int], _ k: Int) {
+		guard nums.count > 1, k > 0 else { return }
+
+		for _ in 1...k {
+			nums.insert(nums.popLast()!, at: 0)
+		}
+	}
+}
