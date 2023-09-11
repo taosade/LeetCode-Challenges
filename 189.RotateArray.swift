1bb99fb3ca60aab297aefc95672ac62301d3557f
Mikhail Melikhov <melihov@me.com>
2023-09-11 12:16:32 +0300

    Update 189.RotateArray.swift

--- 189.RotateArray.swift
+++ 189.RotateArray.swift
@@ -5,11 +5,14 @@ steps, where 'k' is non-negative.
 
 */
 
-class Solution {
-	func rotate(_ nums: inout [Int], _ k: Int) {
+class Solution
+{
+	func rotate(_ nums: inout [Int], _ k: Int)
+	{
 		guard nums.count > 1, k > 0 else { return }
 
-		for _ in 1...k {
+		for _ in 1...k
+		{
 			nums.insert(nums.popLast()!, at: 0)
 		}
 	}
