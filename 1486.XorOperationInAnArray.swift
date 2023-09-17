2dde16b1574d4c1198ecde754fd82c46e2743853
Mikhail Melikhov <melihov@me.com>
2023-09-17 11:08:58 +0300

    Update 1486.XorOperationInAnArray.swift

--- 1486.XorOperationInAnArray.swift
+++ 1486.XorOperationInAnArray.swift
@@ -11,7 +11,8 @@ Return the bitwise XOR of all elements of 'nums'.
 
 class Solution
 {
-	func xorOperation(_ n: Int, _ start: Int) -> Int {
+	func xorOperation(_ n: Int, _ start: Int) -> Int
+	{
 		guard n > 0, start >= 0 else { return 0 }
 
 		let nums = (0...(n - 1)).map { i in start + (i * 2) }
