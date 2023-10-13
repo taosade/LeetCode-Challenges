381bc05a06897bf7549d8ac6a5ecc465c980c9ed
Mikhail Melikhov <melihov@me.com>
2023-10-13 17:20:06 +0300

    Create 1075.ProjectEmployeesI.sql

---
+++ 1075.ProjectEmployeesI.sql
@@ -0,0 +1,15 @@
+/*
+
+Write an SQL query that reports the average experience years of all
+the employees for each project, rounded to 2 digits.
+
+Return the result table in any order.
+
+*/
+
+SELECT
+	Project.project_id,
+	ROUND(AVG(Employee.experience_years), 2) as average_years
+FROM Project
+JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY Project.project_id
