8a1c0e80e2455ddbd3bc516649f1d1c0d48caecc
Mikhail Melikhov <melihov@me.com>
2023-10-15 13:18:36 +0300

    Update 1934.ConfirmationRate.sql

--- 1934.ConfirmationRate.sql
+++ 1934.ConfirmationRate.sql
@@ -12,8 +12,8 @@ Return the result table in any order.
 */
 
 SELECT
-    Signups.user_id,
-    ROUND(AVG(IF(Confirmations.action = "confirmed", 1, 0)), 2) AS confirmation_rate
-FROM Signups
-LEFT JOIN Confirmations ON Confirmations.user_id = Signups.user_id
-GROUP BY Signups.user_id
+    `Signups`.user_id,
+    ROUND(AVG(IF(`Confirmations`.action = 'confirmed', 1, 0)), 2) AS 'confirmation_rate'
+FROM `Signups`
+LEFT JOIN `Confirmations` ON `Confirmations`.user_id = `Signups`.user_id
+GROUP BY `Signups`.user_id
