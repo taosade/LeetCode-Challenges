2bcf594e23a202c4fbcf8c5b16dc8b856c1b3711
Mikhail Melikhov <melihov@me.com>
2023-10-22 19:08:04 +0300

    Create 2665.CounterII.ts

---
+++ 2665.CounterII.ts
@@ -0,0 +1,29 @@
+/*
+
+Write a function 'createCounter'. It should accept an initial integer
+'init'. It should return an object with three functions:
+
+- 'increment()' increases the current value by '1' and then returns
+it.
+- 'decrement()' reduces the current value by '1' and then returns it.
+- 'reset()' sets the current value to 'init' and then returns it.
+
+*/
+
+type ReturnObj =
+{
+	increment: () => number,
+	decrement: () => number,
+	reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj
+{
+	let count = init
+
+	return {
+		increment() { return ++count },
+		decrement() { return --count },
+		reset() { return count = init }
+	}
+}
